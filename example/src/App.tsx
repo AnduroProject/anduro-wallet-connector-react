c503ce7434fec51a972b8883d9d915bd38b6d47d
Hari <hari@bsetxt.com>
2024-04-02 15:23:54 +0530

    updated context changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './route/route';
-import { UseConnectorProvider } from 'anduro-wallet-connecter/dist';
+import { UseConnectorProvider } from 'anduro-wallet-connector';
 
 function App() {
   return (
