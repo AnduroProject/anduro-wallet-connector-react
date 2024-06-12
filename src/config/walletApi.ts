7643cc53ef0355c3a89660461c0776734912d269
daniel <daniel@bsetxt.com>
2024-06-12 15:11:12 +0530

    updated wallet URL

--- src/config/walletApi.ts
+++ src/config/walletApi.ts
@@ -1,1 +1,2 @@
-export const WALLETURL = "chrome-extension://khebhoaoppjeidmdkpdglmlhghnooijn/index.html"
+// export const WALLETURL = "chrome-extension://khebhoaoppjeidmdkpdglmlhghnooijn/index.html"
+export const WALLETURL = "chrome-extension://icplodojmfdeaihdjdlgophfhnjieejc/index.html"
